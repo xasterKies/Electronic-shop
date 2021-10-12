6fe2c77e299e3e8e50d962e97c8d72bcc54bf96c
xaster-Kies <samueledward40@gmail.com>
2021-10-12 13:15:20 +0100

    getting the api base url

--- backend/app.js
+++ backend/app.js
@@ -1,7 +1,15 @@
 const express = require('express')
 const app = express()
 
+require('dotenv/config');
+
+const api = process.env.API_URL
+
+app.get('/', (req, res) => {
+    res.send('Hello API !')
+})
 
 app.listen(3000, () => {
+    console.log(api);
     console.log('Server is running now on localhost 3000')
 })
