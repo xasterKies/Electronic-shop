2aa6ea0f88cd08785a465550be6f709ce46d16a7
xaster-Kies <samueledward40@gmail.com>
2021-10-15 00:33:52 +0100

    category route

--- backend/routes/orders.js
+++ backend/routes/orders.js
@@ -3,7 +3,7 @@ const express = require('express');
 const router = express.Router();
 
 router.get(`/`, async (req, res) => {
-    const userList = await Order.find();
+    const orderList = await Order.find();
 
     if(!orderList) {
         res.status(500).json({success: false})
