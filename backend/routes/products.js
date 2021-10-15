806d8eea63a436539cbe18b624663ab7f82da53f
xaster-Kies <samueledward40@gmail.com>
2021-10-15 08:42:03 +0100

    update product api

--- backend/routes/products.js
+++ backend/routes/products.js
@@ -8,7 +8,7 @@ const Product = require('../models/product')
 router.post(`/`, async (req, res) => {
 
     const category = await Category.findById(req.body.category);
-        if(!category) return res.status(400)
+        if(!category) return res.status(400).send('Invalid Category')
 
     const product = new Product({
         name: req.body.name,
@@ -30,7 +30,6 @@ router.post(`/`, async (req, res) => {
     return res.status(500).send('The Product cannot be created')
 })
 
-
 router.get(`/`, async (req, res) => {
     const productList = await Product.find().select('name image -_id');
 
@@ -45,6 +44,34 @@ router.get(`/:id`, async (req, res) => {
 
     if(!product) {
         res.status(500).json({sucess: false})
+    }
+    res.send(product)
+})
+
+router.put(`/:id`, async (req, res) => {
+    const category = await Category.findById(req.body.category);
+    if(!category) return res.status(400).send('Invalid Category')
+
+    const product = await Product.findByIdAndUpdate(
+        req.params.id,
+        {
+            name: req.body.name,
+            description: req.body.description,
+            richDescription: req.body.richDescription,
+            image: req.body.image,
+            brand: req.body.brand,
+            price: req.body.price,
+            category: req.body.category,
+            countInStock: req.body.countInStock,
+            rating: req.body.rating,
+            numReviews: req.body.numReviews,
+            isFeatured: req.body.isFeatured
+        },
+        {new: true}
+    )
+
+    if(!product) {
+        res.status(500).send('The Product cannot be updated!')
     }
     res.send(product)
 })
