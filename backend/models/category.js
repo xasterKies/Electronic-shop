660fb3519f16268ee12f0c279a934f1245719b43
xaster-Kies <samueledward40@gmail.com>
2021-10-15 01:31:42 +0100

    category Schema

--- backend/models/category.js
+++ backend/models/category.js
@@ -1,7 +1,19 @@
 const mongoose = require('mongoose')
 
 const categorySchema = mongoose.Schema({
-    
+    name: {
+        type: String,
+        required: true,
+    },
+
+    icon: {
+        type: String,
+    },
+
+    color: {
+        type: String
+    }
+
 }) 
 
 exports.Category = mongoose.model('Category', categorySchema)
