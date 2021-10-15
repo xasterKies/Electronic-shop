88aebc3ebb6cef75aba88209b5cc1c6851ad42b4
xaster-Kies <samueledward40@gmail.com>
2021-10-15 02:34:27 +0100

    getting one category

--- backend/routes/categories.js
+++ backend/routes/categories.js
@@ -8,7 +8,17 @@ router.get(`/`, async (req, res) => {
     if (!categoryList) {
         res.status(500).json({ success: false })
     }
-    res.send(categoryList)
+    res.status(200).send(categoryList)
+})
+
+router.get('/:id', async(req, res) => {
+    const category = await Category.findById(req.params.id);
+
+
+    if (!category) {
+        res.status(500).json({ message: 'The category with the given ID was not found' })
+    }
+    res.status(200).send(category)
 })
 
 router.post('/', async (req, res) => {
