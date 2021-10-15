d0da36b510dcdbf5e02c094f29c9a4d396a82704
xaster-Kies <samueledward40@gmail.com>
2021-10-15 01:30:11 +0100

    Product schema done

--- backend/models/product.js
+++ backend/models/product.js
@@ -1,11 +1,65 @@
 const mongoose = require('mongoose')
 
 const productSchema = mongoose.Schema({
-    name: String,
-    image: String,
+    name: {
+        type: String,
+        required: true,
+    },
+
+    description: {
+        type: String,
+        required: true,
+    },
+
+    richDescription: {
+        type: String,
+        default: ''
+    },
+
+    image: {
+        type: String,
+        default: ''
+    },
+
+    images: [{
+        type: String,
+    }],
+
+    brand: {
+        type: String,
+        default: ''
+    },
+
+    price: {
+        type: Number,
+        default: 0
+    },
+
+    category: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'Category'
+    },
+
     countInStock: {
         type: Number,
-        required: true
+        required: true,
+        min: 0,
+        max: 255
+    },
+
+    numReviews: {
+        type: Number,
+        default: 0,
+    },
+
+    isFeatured: {
+        type: Boolean,
+        default: false,
+    },
+
+    dateCreated: {
+        type: Date,
+        default: Date.now
     }
 }) 
 
