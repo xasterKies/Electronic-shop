3c3e27eead1790db8bb7edca976efe7bde13cd2a
xaster-Kies <samueledward40@gmail.com>
2021-10-16 18:44:00 +0100

    admins shit i did not really get

--- backend/routes/users.js
+++ backend/routes/users.js
@@ -58,7 +58,8 @@ router.post('/login', async(req, res) => {
     if(user && bcrypt.compareSync(req.body.password, user.passwordHash)) {
         const token = jwt.sign(
             {
-                userId: user.id
+                userId: user.id,
+                isAdmin: user.isAdmin
             },
             secret,
             {expiresIn: '1d'}
