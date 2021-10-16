7ba9263f8b84221320bc29c4e5006ca76a7e6f4c
xaster-Kies <samueledward40@gmail.com>
2021-10-16 18:37:50 +0100

    enclosing app in auth and hiding somethings in the api

--- backend/helpers/jwt.js
+++ backend/helpers/jwt.js
@@ -2,10 +2,18 @@ const expressJwt = require('express-jwt');
 
 function authJwt() {
     const secret = process.env.SECRET
+    const api = process.env.API_URL
     return expressJwt({
         secret,
         algorithms: ['HS256']
-    } )
+    }).unless({
+        path: [
+            { url: /\/api\/v1\/products(.*)/, methods: ['GET', 'OPTIONS'] },
+            { url: /\/api\/v1\/categories(.*)/, methods: ['GET', 'OPTIONS'] },
+            `${api}/users/login`,
+            `${api}/users/register`
+        ]
+    })
 }
 
 module.exports = authJwt;
