69c69b1c78903bb8db937b3337fa8aac96690e4e
xaster-Kies <samueledward40@gmail.com>
2021-10-16 14:10:14 +0100

    hashed the password

--- backend/routes/users.js
+++ backend/routes/users.js
@@ -2,6 +2,29 @@ const {User} = require('../models/user');
 const express = require('express');
 const router = express.Router();
 
+router.post('/', async (req, res) => {
+    let user = new User({
+        name: req.body.name,
+        email: req.body.email,
+        color: req.body.color,
+        passwordHash: bcrypt.hashSync(req.body.password, 10),
+        phone: req.body.phone,
+        isAdmin: req.body.isAdmin,
+        street: req.body.street,
+        apartment: req.body.apartment,
+        zip: req.body.zip,
+        city: req.body.city,
+        country: req.body.country
+
+    })
+    user = await category.save();
+
+    if(!user)
+    return res.status(404).send('The user cannot be created!')
+
+    res.send(category)
+})
+
 router.get(`/`, async (req, res) => {
     const userList = await User.find();
 
