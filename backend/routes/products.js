84076f1041ae1f44857c64c06d0dfaa089003bb6
xaster-Kies <samueledward40@gmail.com>
2021-10-16 12:52:59 +0100

    limiting featured products in shop

--- backend/routes/products.js
+++ backend/routes/products.js
@@ -94,8 +94,9 @@ router.delete('/:id', (req, res) => {
     })
 })
 
-router.get(`/get/featured`, async (req, res) => {
-    const product = await Product.find({isFeatured: true}) 
+router.get(`/get/featured/:count`, async (req, res) => {
+    const count = req.params.count ? req.params.count : 0
+    const product = await Product.find({isFeatured: true}).limit(+count)
 
     if(!product) {
         res.status(500).json({sucess: false})
