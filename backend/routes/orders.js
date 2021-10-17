81bdea6973f79d8c95426df2abf805194d2fd09b
xaster-Kies <samueledward40@gmail.com>
2021-10-17 18:11:09 +0100

    Deleting an order

--- backend/routes/orders.js
+++ backend/routes/orders.js
@@ -18,7 +18,7 @@ router.get(`/:id`, async (req, res) => {
     .populate({ 
         
         path: 'orderItems', populate: {
-            path: 'product', populate: 'category'}
+            path: 'product', populate: 'order'}
         })
 
     if(!order) {
@@ -76,5 +76,16 @@ router.put('/:id' ,async (req, res) => {
     res.send(order)
 })
 
+router.delete('/:id', (req, res) => {
+    Order.findByIdAndRemove(req.params.id).then(order => {
+        if(order) {
+            return res.status(200).json({success: true, message: 'The order is deleted'})
+        } else {
+            return res.status(404).json({success: false, message: 'order not found'})
+        }
+    }).catch(err => {
+        return res.status(400).json({success: false, error: err})
+    })
+})
 
 module.exports = router;
