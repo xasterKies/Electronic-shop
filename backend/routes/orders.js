90a75c295ed8c08818eb05a0a198f72d9efb1ce2
xaster-Kies <samueledward40@gmail.com>
2021-10-17 07:36:47 +0100

    oder item resolved

--- backend/routes/orders.js
+++ backend/routes/orders.js
@@ -1,5 +1,6 @@
 const {Order} = require('../models/order');
 const express = require('express');
+const { OrderItem } = require('../models/order-item');
 const router = express.Router();
 
 router.get(`/`, async (req, res) => {
@@ -10,5 +11,41 @@ router.get(`/`, async (req, res) => {
     }
     res.send(orderList)
 })
+
+router.post('/', async (req, res) => {
+    const orderItemsIds = Promise.all(req.body.orderItems.map( async orderitem => {
+        let newOrderItem = new OrderItem({
+            quantity: orderitem.quantity,
+            product: orderitem.product
+        })
+
+        newOderItem = await newOderItem.save();
+
+        return newOderItem._id;
+    }))
+    const orderItemsIdsResolved = await orderItemsIds
+
+
+
+    let order = new Order({
+        orderItems: orderItemsIdsResolved,
+        shippingAddress1: req.body.shippingAddress1,
+        shippingAddress2: req.body.shippingAddress2,
+        city: req.body.city,
+        zip: req.body.zip,
+        country: req.body.country,
+        phone: req.body.phone,
+        status: req.body.status,
+        totalPrice: req.body.totalPrice,
+        user: req.body.user
+    })
+    order = await order.save();
+
+    if(!order)
+    return res.status(404).send('The oreder cannot be created!')
+
+    res.send(order)
+})
+
 
 module.exports = router;
