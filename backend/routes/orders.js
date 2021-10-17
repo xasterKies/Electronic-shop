01b845f9abd245534be067de0e3fcff71f7eac2f
xaster-Kies <samueledward40@gmail.com>
2021-10-17 18:04:59 +0100

    order a product by item and category

--- backend/routes/orders.js
+++ backend/routes/orders.js
@@ -4,12 +4,27 @@ const { OrderItem } = require('../models/order-item');
 const router = express.Router();
 
 router.get(`/`, async (req, res) => {
-    const orderList = await Order.find();
+    const orderList = await Order.find().populate('user', 'name').sort({'dateOrdered' : -1 });
 
     if(!orderList) {
         res.status(500).json({success: false})
     }
     res.send(orderList)
+})
+
+router.get(`/:id`, async (req, res) => {
+    const order = await Order.findById(req.params.id)
+    .populate('user', 'name')
+    .populate({ 
+        
+        path: 'orderItems', populate: {
+            path: 'product', populate: 'category'}
+        })
+
+    if(!order) {
+        res.status(500).json({success: false})
+    }
+    res.send(order)
 })
 
 router.post('/', async (req, res) => {
@@ -24,8 +39,6 @@ router.post('/', async (req, res) => {
         return newOderItem._id;
     }))
     const orderItemsIdsResolved = await orderItemsIds
-
-
 
     let order = new Order({
         orderItems: orderItemsIdsResolved,
